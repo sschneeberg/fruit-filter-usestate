188c5d971ff7410169da5bca59f346670a191d13
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-10 15:11:44 -0500

    Addcomments

--- src/Components/FruitContainer.js
+++ src/Components/FruitContainer.js
@@ -3,6 +3,7 @@ import List from './List';
 import Input from './Input';
 
 function FruitContainer(props) {
+    //normally you declare one hook per each state variable rather than an object
     const [state, setState] = useState({
         //initialize fruit list to the full list
         fruitToDisp: props.fruits,
@@ -11,7 +12,8 @@ function FruitContainer(props) {
     });
 
     /* YOU CAN DEFINE FUNCTION IN FUNCTION IN JS */
-    function handleChange(e) {
+    //must assign toa variable
+    const handleChange = (e) => {
         e.preventDefault();
         let filterValue = e.target.value.toLowerCase();
         const filteredFruits = props.fruits.filter((fruit) => {
@@ -21,7 +23,7 @@ function FruitContainer(props) {
             filter: filterValue,
             fruitToDisp: filteredFruits
         });
-    }
+    };
 
     return (
         <div className="Fruit-Container">
